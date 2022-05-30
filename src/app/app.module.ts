c4a5e68811ca1ee20e45c47cae9ee93428abed19
57ed <repostudyoutube@outlook.com>
2022-05-30 06:33:25 -0300

    event

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { FirstComponent } from './components/first/first.component';
 import { ParentDataComponent } from './components/parent-data/parent-data.component';
 import { DirectivesComponent } from './components/directives/directives.component';
 import { IfRenderComponent } from './components/if-render/if-render.component';
+import { EventosComponent } from './components/eventos/eventos.component';
 
 @NgModule({
-  declarations: [AppComponent, FirstComponent, ParentDataComponent, DirectivesComponent, IfRenderComponent],
+  declarations: [AppComponent, FirstComponent, ParentDataComponent, DirectivesComponent, IfRenderComponent, EventosComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
