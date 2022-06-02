94052ff8e1389d18aafb1b6b7db70f2590fbabe7
57ed <repostudyoutube@outlook.com>
2022-06-01 22:32:28 -0300

    router #17, navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FirstComponent } from './components/first/first.component';
+import { ListRenderComponent } from './components/list-render/list-render.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: FirstComponent },
+  { path: 'list', component: ListRenderComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
